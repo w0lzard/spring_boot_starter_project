668ca2e900a0d5a0a76482f8d245ac4386128fbf
w0lzard <kpsingj6@gmail.com>
2026-01-12 23:48:05 +0530

    Enhance JWTAuthFilter to set SecurityContext using validated tokens

--- src/main/kotlin/com/ryuken/spring_boot_crash_course/security/JWTAuthFilter.kt
+++ src/main/kotlin/com/ryuken/spring_boot_crash_course/security/JWTAuthFilter.kt
@@ -3,6 +3,8 @@ package com.ryuken.spring_boot_crash_course.security
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
@@ -11,10 +13,18 @@ class JWTAuthFilter(
     private val jwtService: JWTService
 ): OncePerRequestFilter() {
     override fun doFilterInternal(
-        request: HttpServletRequest?,
-        response: HttpServletResponse?,
-        filterChain: FilterChain?
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
     ) {
         val authHeader = request.getHeader("Authorization")
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            if(jwtService.validateAccessToken(authHeader)) {
+                val userId = jwtService.getUserIdFromToken(authHeader)
+                val auth = UsernamePasswordAuthenticationToken(userId, null)
+                SecurityContextHolder.getContext().authentication = auth
+            }
+        }
+        filterChain.doFilter(request, response)
     }
 }
